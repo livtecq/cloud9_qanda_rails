55d7a6887747d35aa69de5f40fe6015a53ecb620
EC2 Default User <ec2-user@ip-172-31-6-146.ap-northeast-1.compute.internal>
2021-11-16 12:22:02 +0000

    ルーティングの設定/Question

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'questions/index'
-  get 'questions/show'
-  get 'questions/new'
-  get 'questions/edit'
+  # get 'questions/index'
+  # get 'questions/show'
+  # get 'questions/new'
+  # get 'questions/edit'
+  
+  # resourcesを使用したルーティングの設定
+  resources :puestions
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
